f9f3e3d66fba0765d990fb0ba982f906ef1fe225
Débora de Oliveira <deboramartinsdeoliveira2008@gmail.com>
2026-08-21 14:20:04 -0300

    fix(card): método de pegar as iniciais

--- src/utils/FGetInitials.tsx
+++ src/utils/FGetInitials.tsx
@@ -1,5 +1,5 @@
 export function getInitials(name: string): string {
-  const nameParts = name.split(" ");
-  const initials = nameParts.map((part) => part.charAt(0).toUpperCase());
-  return initials.join("");
+  const initial= name.slice(0, 1).toUpperCase()
+  const initialSecond = name.split(" ")[1].slice(0, 1).toUpperCase()
+  return initial + initialSecond;
 }
